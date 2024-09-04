c29b40f9eb0406ac52dba897f1b68db6c17922c5
Ahmed Essam <63825434+aahmedessam30@users.noreply.github.com>
2024-09-04 12:34:03 +0300

    Fix Localization

--- src/Middleware/LocalizationMiddleware.php
+++ src/Middleware/LocalizationMiddleware.php
@@ -7,8 +7,8 @@ class LocalizationMiddleware
     public function handle($request, $next)
     {
         config([
-            'app.locale'          => $request->header('Accept-Language', config('app.locale')),
-            'app.fallback_locale' => $request->header('Accept-Fallback-Language', config('app.fallback_locale')),
+            'app.locale'          => $request->header('Language', config('app.locale')),
+            'app.fallback_locale' => $request->header('Fallback-Language', config('app.fallback_locale')),
             'app.timezone'        => $request->header('Accept-Timezone', config('app.timezone'))
         ]);
 
